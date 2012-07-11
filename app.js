23a169e7e294cebf1061657d327b9d94ec42761d
JorgeLeon <jorge_agl@hotmail.com>
2012-07-11 18:17:32 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+var express = require("express");
+var app = express.createServer({host:'http://nsliders.nodester.com/'});
+var io = require('socket.io').listen(app);
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('slides.jade');
+});
+
+app.get('/manage', function(req, res){
+  res.render('manage.jade');
+});
+
+
+
+var lastSlideId = 1;
+
+var slidesSockets = io.of("/slides");
+var manageSockets = io.of("/manage");
+
+manageSockets.on("connection", function(socket) {
+
+  socket.on("changeto", function(slideId) {
+    lastSlideId = slideId;
+    slidesSockets.emit("changeto", slideId);
+  });
+
+});
+
+
+
+slidesSockets.on("connection", function(socket) {
+  socket.emit("startfrom", lastSlideId);
+});
+
+
+
+app.listen(19645);
+console.log("Listening on http://nsliders.nodester.com/:19645/");
